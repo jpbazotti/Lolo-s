cb1cc667fca377a325fac6df0d5f5c5f470b64c9
Eu não ligo kk <izaiassaturninodelimaneto@hotmail.com>
2020-11-04 16:42:11 -0300

    Update gravacao.c

--- gravacao.c
+++ gravacao.c
@@ -5,3 +5,14 @@ typedef gravacao {
 	int vidas;
 	char nomejogador[9];
 } gravacao;
+
+void geraGravacao(gravacao save){
+	FILE *file;
+	if(!(file = fopen("save.bin","ab"))){
+		printf("Erro de leitura!\n");
+	}
+	if (fwrite(&save, sizeof(gravacao), 1, file) != 1){
+		printf("Erro de escrita!\n");
+	}
+	fclose(file);
+}
