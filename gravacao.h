5e43ef2dd7049208db56717184c425b0ed9d0d52
Izaias Neto <izaiassaturninodelimaneto@hotmail.com>
2020-11-06 17:49:13 -0300

    getSave

--- gravacao.h
+++ gravacao.h
@@ -31,3 +31,22 @@ int getLast(){
 	fclose(file);
 	return pos;
 }
+
+gravacao getSave(int pos){
+	File *file;
+	gravacao save;
+	if(!(file = fopen("save.bin","rb"))){
+		printf("Erro de leitura!\n");
+	}
+	rewind(file);
+	if(fread(&save, sizeof(funcionario_st), pos-1, file) == 1){
+		printf("Erro de leitura!\n");
+	}
+	if(save.id == pos){
+		printf("Tudo certo muleque!\n");
+	}else{
+		printf("Erro de leitura!\n");
+	}
+	fclose(file);
+	return save;
+}
