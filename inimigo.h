a47a6d9060a60a98e3e41cdc93753f6d350db96a
Izaias Neto <izaiassaturninodelimaneto@hotmail.com>
2020-11-09 17:54:13 -0300

    moveEnemies

--- inimigo.h
+++ inimigo.h
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include "nivel.h"
 //struc gravacao
 typedef struct
 {
 	int vida;
 	int movimento;
-	int x;
-	int y;
+	ponto_st posicao;
 } enemy;
 
 enemy* getEnemies(char nivel[][13]){
@@ -24,4 +25,39 @@ enemy* getEnemies(char nivel[][13]){
 		}
 	}
 	return enemies;
+}
+
+void moveEnemies(char nivel[][13]){
+	srand(time(NULL));
+	enemy enemies[] = getEnemies(nivel);
+	int qtd = sizeof(enemies)/sizeof(enemy);
+	int i = 0;
+	int move = -1;
+	ponto_st novaPos;
+	for (i = 0; i < qtd; i++){
+		if(enemies[i].movimento == 1){
+			move = rand()%4;
+			enemies[i].movimento = 0;
+			if(move == 0){
+		        novaPos.x = enemies[i].posicao.x;
+		        novaPos.y = enemies[i].posicao.y - 1;
+			}else if(move == 1){
+				novaPos.x = jogador.posicao.x-1;
+		        novaPos.y = jogador.posicao.y;
+			}else if(move == 2){
+				novaPos.x = jogador.posicao.x;
+		        novaPos.y = jogador.posicao.y + 1;
+			}else if(move == 3){
+				novaPos.x = jogador.posicao.x + 1;
+		        novaPos.y = jogador.posicao.y;
+			}
+		    if(nivel[novaPos.x][novaPos.y] != ' '){
+				novaPos.x = enemies[i].posicao.x;
+			    novaPos.y = enemies[i].posicao.y;
+			    enemies[i].movimento = 1;
+			}
+			nivel[enemies[i].posicao.x][enemies[i].posicao.y] = ' ';
+			nivel[novaPos.x][novaPos.y] = 'E';
+		}
+	}
 }
